e7c69ebdbfb7918a06ef183b82ef5345a1ba3790
tungbe230402 <tungbe230402@gmail.com>
2024-08-01 17:57:45 +0700

    Update Statistical

--- WebBanHangOnline/Models/EF/Product.cs
+++ WebBanHangOnline/Models/EF/Product.cs
@@ -42,6 +42,7 @@ namespace WebBanHangOnline.Models.EF
 
         [StringLength(250)]
         public string Image { get; set; }
+        public decimal OriginalPrice { get; set; }
         public decimal Price { get; set; }
         public decimal? PriceSale { get; set; }
         public int Quantity { get; set; }
